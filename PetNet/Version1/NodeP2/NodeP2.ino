e42337c8836fc68096c8e0b99b74d320878b7bed
Greg <34717752+gturney@users.noreply.github.com>
2022-04-25 17:09:41 -0400

    Update NodeP2.ino

--- PetNet/Version1/NodeP2/NodeP2.ino
+++ PetNet/Version1/NodeP2/NodeP2.ino
@@ -10,7 +10,7 @@
            esp32 Dev Board =   Pet1          682343137     0x24, 0x6F, 0x28, 0xAB, 0xBA, 0xE0
        TTGO damaged Screen =   NodeF1        682617761     0x24, 0x6F, 0x28, 0xAF, 0xEB, 0xA0
        TTGO broken Screen  =   NodeP2        680951025     0x24, 0x6F, 0x28, 0x96, 0x7C, 0xF0
-      esp32 mini Dev Board =   NodeR1        xxxxxxxxx        
+      esp32 mini Dev Board =   NodeR1        533118137     9C:9C:1F:C6:BC:B8        
       esp32 mini Dev Board =   NodeR2        533317941     9C:9C:1F:C9:C9:34
 */
 
